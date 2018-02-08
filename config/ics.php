36fac1bda4a2c754d0073f0bbe79e86be49e4a33
Andreas Born <futur.andy@googlemail.com>
2018-02-08 19:35:54 +0100

    Config: Make ics config configurable through .env

--- config/ics.php
+++ config/ics.php
@@ -2,10 +2,9 @@
 
 return [
 
-    'fpi' => env('APP_FPI', '-//phoosha//Dienstplan//EN'),
-    'domain' => 'localhost',
-    'organizer' => 'mailto:mail@example.com',
-    'summary' => 'Dienst FRS',
-    'location' => 'Musterheim',
+    'fpi' => env('ICS_API', '-//Phoosha//Dienstplan//EN'),
+    'organizer' => env('ICS_ORGANIZER', 'mailto:mail@example.com'),
+    'summary' => env('ICS_SUMMARY', 'Dienst FRS'),
+    'location' => env('ICS_LOCATION', ':slot, Musterheim'),
 
 ];
--- resources/views/api/duties.blade.php
+++ resources/views/api/duties.blade.php
@@ -13,7 +13,7 @@ X-WR-CALNAME:{{ $cal_name }}
 X-WR-TIMEZONE:{{ config('app.timezone')  }}
 @foreach ($duties as $duty)
 BEGIN:VEVENT
-UID:{{ sha1($duty->id.$duty->created_on) }}{{ '@' . config('ics.domain') }}
+UID:{{ sha1($duty->id.$duty->created_on) }}{{ '@' . URL::formatRoot('') }}
 DTSTAMP:{{ icsZuluDateTime($duty->updated_at) }}
 DTSTART:{{ icsZuluDateTime($duty->start) }}
 DTEND:{{ icsZuluDateTime($duty->end) }}
@@ -22,7 +22,7 @@ CREATED:{{ icsZuluDateTime($duty->created_at) }}
 SEQUENCE:{{ $duty->sequence }}
 SUMMARY:{{ config('ics.summary') }}
 ORGANIZER:{{ config('ics.organizer') }}
-LOCATION:{{ $duty->slot->name }}, {{ config('ics.location') }}
+LOCATION:{{ str_replace(':slot', $duty->slot->name, config('ics.location')) }}
 RESOURCES:{{ $duty->slot->name }}
 DESCRIPTION:{{ $duty->type === App\Duty::WITH_INTERNEE ? 'Dienst mit Praktikant' : '' }}
 URL:{{ url('duties', $duty->id) }}
