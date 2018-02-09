52b0a7443daf7e3db85b6f0bf48c9addaf2df0df
Andreas Born <futur.andy@googlemail.com>
2018-02-09 22:24:26 +0100

    Add a minimal, now default production seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,21 +1,30 @@
 <?php
 
+use App\SlotConfig;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        $this->call([
-            UsersTableSeeder::class,
-            PostsTableSeeder::class,
-            SlotsTableSeeder::class,
-            PhonesTableSeeder::class,
+    public function run() {
+        User::create([
+            'first_name' => 'Admin',
+            'last_name' => 'Istrator',
+            'login' => 'admin',
+            'email' => 'admin@example.org',
+            'phone' => '0176 917423',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'is_admin' => true,
         ]);
+        SlotConfig::createWithSlots(
+            Carbon::instance(config('dienstplan.min_date')),
+            [ '79/1', '10/1' ]
+        );
     }
 }
---
+++ database/seeds/DevDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DevDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            UsersTableSeeder::class,
+            PostsTableSeeder::class,
+            SlotsTableSeeder::class,
+            PhonesTableSeeder::class,
+        ]);
+    }
+}
