e1ef78f3c86230c1a0d9f95e23cb545799c208ff
Andreas Born <futur.andy@googlemail.com>
2018-02-24 16:10:06 +0100

    Add auth middleware to Admin Controller draft

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 class AdminController extends Controller {
 
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function reports() {
         return "FIXME";
     }
